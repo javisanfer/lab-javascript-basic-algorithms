1936a7c831456d43545d6dc101de0d3165a769e5
javisanfer <164020466+javisanfer@users.noreply.github.com>
2024-09-26 22:58:25 +0200

    Iteration 1 and Iteration 2

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 // Iteration 1: Names and Input
+const hacker1 = 'David';
+const hacker2 = 'Sara';
 
+console.log(`The drive's name is ${hacker1}`);
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+if(hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker2.length > hacker1.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
+}
 
 // Iteration 3: Loops
+
+// 3.1
+
+console.log(hacker1.toUpperCase().split(''))
+
+// 3.2
+
+const reverseName;
+
+for () {
+    
+}
+// 3.2
