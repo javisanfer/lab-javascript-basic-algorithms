5ec80ef8a2139eefb77c599e79d08fab0b586353
javisanfer <164020466+javisanfer@users.noreply.github.com>
2024-09-28 16:26:09 +0200

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Iteration 1: Names and Input
-const hacker1 = 'David';
-const hacker2 = 'Sara';
+const hacker1 = 'Javi';
+const hacker2 = 'Julia';
 
 console.log(`The drive's name is ${hacker1}`);
 console.log(`The navigator's name is ${hacker2}`);
@@ -19,13 +19,29 @@ if(hacker1.length > hacker2.length) {
 
 // 3.1
 
-console.log(hacker1.toUpperCase().split(''))
-
-// 3.2
-
-const reverseName;
-
-for () {
-    
+let hacker1UpperCase = '';
+for (let i = 0; i < hacker1.length; i++) {
+    hacker1UpperCase += hacker1[i].toUpperCase() + ' ';
 }
+console.log(hacker1UpperCase.trim());
+
 // 3.2
+
+let reverseName = '';
+for (let i = hacker1.length-1; i >= 0; i--) {
+    reverseName += hacker1[i];
+}
+console.log(reverseName);
+
+// 3.2
+
+const hacker1Lower = hacker1.toLowerCase();
+const hacker2Lower = hacker2.toLowerCase();
+
+if (hacker1Lower < hacker2Lower) {
+    console.log(`The driver's name goes first.`);
+} else if (hacker1Lower > hacker2Lower) {
+    console.log(`Yo, the navigator goes first, definitely.`);
+} else {
+    console.log(`What?! You both have the same name?`)  
+}
